ece64e3ba2a025bf7fdc492351e5b6284ef7da0a
Nagarajan K <nagrks@yahoo.com>
2025-11-22 20:07:03 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Demo for Azure website ........
+          After deployment in Azure
         </p>
         <a
           className="App-link"
