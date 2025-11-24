72557ea5e59d11caaf674c514d49ffd426308ae9
unknown <nagrks@yahoo.com>
2025-11-24 11:18:21 +0530

    This is after third check into GitHub repo

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Demo for Azure website ........
+          After deployment in Azure .....
+          This is after third check into GitHub repo.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
